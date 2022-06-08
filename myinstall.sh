d8ae8cd268302614618e38da50d1c33f07d8232f
vladskvortsov <vladskvortsov96@gmail.com>
2022-06-08 16:25:02 +0300

    docker, compose, telegram works on docker-ubuntu image

--- myinstall.sh
+++ myinstall.sh
@@ -7,33 +7,38 @@ apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt update
-apt-cache policy docker-ce
 apt install docker-ce
-systemctl status docker
 usermod -aG docker ${USER}
 id -nG
+
+docker --version
+
 
 #Docker-compose
 
 curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
 docker-compose --version
 
-#Spotify
-
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com стабільний небезкоштовний" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
-
-#Google Chrome
-
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update 
-sudo apt-get install google-chrome-stable
- 
 #Telegram Desktop
 
 sudo apt install telegram-desktop
 
-#
+
+#Spotify(not working)
+
+# curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+# echo "deb http://repository.spotify.com стабільний небезкоштовний" | sudo tee /etc/apt/sources.list.d/spotify.list
+# sudo apt-get update && sudo apt-get install spotify-client
+
+#Google Chrome (not working)
+
+# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# sudo apt-get update 
+# sudo apt-get install google-chrome-stable
+
+
+exit 1
+
