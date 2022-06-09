37656681209cf8c37e320fbdbdcc1dbc3b869f11
vladskvortsov <vladskvortsov96@gmail.com>
2022-06-09 15:46:53 +0300

    all new versions

--- myinstall.sh
+++ myinstall.sh
@@ -39,7 +39,12 @@ docker --version
 
 #Docker-compose
 
-curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt install docker-compose-plugin
 chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
