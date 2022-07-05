ad5e5acf1085ecd935fce9e6b67e7aec2a098fe2
vladskvortsov <vladskvortsov96@gmail.com>
2022-07-06 00:57:45 +0300

    description updated

--- myinstall.sh
+++ myinstall.sh
@@ -2,9 +2,9 @@
 
 
  #############################################################################
- #                                                                           #
-  #  myinstall.sh - installs soft on Ubuntu/Debian. Simple menu,            #
- #   actual versions. Adapted for docker images. May need run with sudo.     # 
+ #   myinstall.sh - installs soft on Ubuntu/Debian. Simple menu,             #
+  #  actual versions. Adapted for docker images. Should run with sudo.      #
+ #   Use "space" to choose a few "1 2 5" etc..                               #
   #   Software included:                                                    #
  #     -Docker                                                               #
   #    -Docker-compose                                                      #
