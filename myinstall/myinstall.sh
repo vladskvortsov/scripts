6ce5409d4e44b5a9c9f3fbd994b01a6464419e19
vladskvortsov <vladskvortsov96@gmail.com>
2022-10-15 22:38:26 +0300

    update progress added

--- myinstall/myinstall.sh
+++ myinstall/myinstall.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-apt-get update
-apt-get install dialog
-
-
-
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 TURQUOISE="\033[0;96m"
@@ -13,9 +8,9 @@ NOCOLOR="\033[0m"
 
 
 
-display_progress(){
+update_progress(){
 
-  out=$( apt-get update -y -qq -o=Dpkg::Use-Pty=0 && $1 )
+  out=$( apt-get update -y -qq -o=Dpkg::Use-Pty=0 && apt-get install dialog )
   percent=0
 
   (
@@ -24,6 +19,33 @@ display_progress(){
   echo $percent
   echo "$out"
   percent=`expr $percent + 1`
+  done
+  ) | dialog --title "$(grep PRETTY_NAME /etc/os-release | cut -d '"' -f 2- | cut -d '"' -f -1)" --gauge "Updating packages..
+" 10 60 0
+
+
+}
+
+
+
+
+
+
+
+
+
+
+display_progress(){
+
+  out=$( $1 )
+  percent=0
+
+  (
+  while test $percent != 101
+  do
+    echo $percent
+    echo "$out"
+    percent=`expr $percent + 1`
   done
   ) | dialog --title "Progress" --gauge "Installing:
   $result1
@@ -34,10 +56,11 @@ display_progress(){
   $result6
   $result7
   $result8
-" 10 60 0
+  " 10 60 0
 
 
 }
+
 
 
 display_result(){
@@ -87,7 +110,7 @@ fi
 
 function Docker(){
 
-apt-get install -y sudo apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-utils sudo apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-get update -y
@@ -104,7 +127,7 @@ check_installedv "                      $(docker --version)
 
 function Docker-compose(){
 
-apt-get install -y sudo apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-utils sudo apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-get update -y
@@ -209,6 +232,12 @@ $(apt-cache policy virtualbox)
 }
 
 
+
+Menu(){
+
+update_progress
+
+
 cmd=(dialog --separate-output --checklist "What would you like to install?" 22 76 16)
 
 options=(1 "Docker" off
@@ -265,7 +294,9 @@ continue
       ;;
 
       5) result5=$(check_installedv "
-      $(spotify -version)
+      $(spotify -version)air.sh
+vlad@vlad-HP-ProBook-640-G1:~/scripts$ cd myinstall/
+
                                  " spotify-client)
 
     display_progress Spotify-client
@@ -313,6 +344,7 @@ continue
                                                              " docker-compose-plugin)
 
        result3=$(check_installedv "
+
        $(apt-cache policy telegram-desktop)
                                                              " telegram-desktop)
        result4=$(check_installedv "
@@ -345,13 +377,191 @@ continue
 
       ;;
 
+
+
+
     esac
 
 
     exit 1
 
+
+
  done
 
  display_result
+
+}
+
+
+Reserve_menu(){
+
+
+#  while true
+#  do
+
+  echo -e "${TURQUOISE}
+  #############################################################################
+  #   myinstall.sh - installs soft on Ubuntu/Debian. Simple menu,             #
+   #  actual versions. Adapted for docker images. Should run with sudo.      #
+  #   Use "space" to choose a few "1 2 5" etc..                                   #
+  #############################################################################
+    ${NOCOLOR}"
+  echo "What would you like to install?
+                            "
+  echo "1) Docker"
+  echo "2) Docker-compose"
+  echo "3) Telegram Desktop"
+  echo "4) Google Chrome"
+  echo "5) Spotify"
+  echo "6) Atom"
+  echo "7) Qbittorrent"
+  echo "8) Virtualbox"
+  echo "9) All of it.."
+  echo "10) Exit"
+
+  read -p "
+
+  Choose number(1-7):
+       " -a array
+   for choice in "${array[@]}"; do
+
+
+  case $choice in
+
+  [1]* ) apt-get update -y && Docker
+
+  check_installedv "
+  $(docker --version)
+                              " docker-ce
+
+   ;;
+
+  [2]* ) apt-get update -y && Docker-compose
+
+  check_installedv "
+  $(docker compose version)
+                             " docker-compose-plugin
+
+  ;;
+
+  [3]* ) apt-get update -y && Telegram-Desktop
+
+  check_installedv "
+  $(apt-cache policy telegram-desktop)
+                             " telegram-desktop
+
+   ;;
+
+
+  [4]* ) apt-get update -y && Google-Chrome
+
+  check_installedv "
+  $(google-chrome-stable --version)
+                                  " google-chrome-stable
+
+  ;;
+
+  [5]* ) apt-get update -y && Spotify-client
+
+  check_installedv "
+  $(spotify -version)
+                             " spotify-client
+
+  ;;
+
+  [6]* ) apt-get update -y && Atom
+
+  check_installedv "
+  $(atom -version)
+                             " atom
+
+  ;;
+
+  [7]* ) apt-get update -y && Qbittorrent
+
+  check_installedv "
+  $(qbittorrent -v)
+                             " qbittorrent
+
+  ;;
+
+  [8]* ) apt-get update -y && Virtualbox
+
+  check_installedv "
+  $(apt-cache policy virtualbox)
+                                                        " virtualbox
+
+  ;;
+
+
+  [9]* ) apt-get update -y && Docker
+  Docker-compose
+  Telegram-Desktop
+  Google-Chrome
+  Spotify-client
+  Atom
+  Qbittorrent
+  Virtualbox
+
+  check_installedv "
+  $(docker --version)
+                              " docker-ce
+
+  check_installedv "
+  $(docker compose version)
+                             " docker-compose-plugin
+
+  check_installedv "
+  $(apt-cache policy telegram-desktop)
+                             " telegram-desktop
+
+  check_installedv "
+  $(google-chrome-stable --version)
+                                  " google-chrome-stable
+
+  check_installedv "
+  $(spotify -version)
+                             " spotify-client
+
+
+  check_installedv "
+  $(atom -version)
+                             " atom
+  check_installedv "
+  $(qbittorrent -v)
+                             " qbittorrent
+
+  ;;
+
+  [10]* ) break
+
+  #Exit
+
+  ;;
+
+  *) continue
+
+  ;;
+
+    esac
+
+  done
+
+    exit 1
+
+
+#done
+
+}
+
+Menu
+
+#Reserve_menu
+
+
+
+
+
 
 #end
